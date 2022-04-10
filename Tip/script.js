74fddffd9280ef7d56d50d3526cf799c0c2cc410
Kris Syevens <98124840+Kris-Syevens@users.noreply.github.com>
2022-04-09 19:04:15 -0500

    Update Commit
    Updated the host folders for 3Col, Product, and Tip repos.

--- Tip/script.js
+++ Tip/script.js
@@ -7,7 +7,10 @@ submitAction.onclick = function () {
     var billAmount = document.querySelector("#billAmount").value;
     var tipPercentInput = document.querySelector("#tipPercent").value;
     var tipPercent = (tipPercentInput / 100);
-    var tipResults = ("$" + (billAmount * tipPercent));
+    var tipNumber = billAmount * tipPercent;
+    var tipResults = ("$" + tipNumber.toFixed(2));
+
+    
    
     return document.querySelector(".tip-result").innerText = (tipResults);
 }
