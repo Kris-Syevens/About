70264a9e789b3d9ce50177749e9ed8461eda6da1
Kris Syevens <98124840+Kris-Syevens@users.noreply.github.com>
2022-04-29 17:38:43 -0500

    Update Commit
    Removed all directories for projects hosted as there were unnecessary copies.

--- Navbar/app.js
+++
@@ -1,6 +0,0 @@
-const linksContainer = document.querySelector(".links");
-const toggleBtn = document.querySelector(".nav-toggle");
-
-toggleBtn.addEventListener("click", function () {
-  linksContainer.classList.toggle("show-links");
-});
