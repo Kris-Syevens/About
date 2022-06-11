3e222d428092583f50eed8b6a111108e40b86537
Kris <kris@syevens.com>
2022-06-10 20:43:25 -0500

    Initial Commit

---
+++ Repo-Files/30-Days-Python/2-Python.py
@@ -0,0 +1,32 @@
+# Day 2 of 30
+
+# *** Python Data Types ***
+
+# Built-in Data Types
+# In programming, data type is an important concept.
+
+# Variables can store data of different types, and different types can do different things.
+
+# Python has the following data types built-in by default, in these categories:
+
+# Text Type:	    str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	    dict
+# Set Types:	    set, frozenset
+# Boolean Type:	    bool
+# Binary Types:	    bytes, bytearray, memoryview
+# None Type:	    NoneType
+
+# To check the type of a variable or object, you can use the type() method.
+
+a = "Kris" # String
+b = 2 # Int
+c = 2.0 # Float
+d = 2j # Complex
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
