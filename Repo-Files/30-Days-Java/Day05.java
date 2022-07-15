6226ccc029c878d6a0b07c6e8db0e6e980044758
Kris <kris@syevens.com>
2022-07-14 19:04:16 -0500

    Update Commit - Java

---
+++ Repo-Files/30-Days-Java/Day05.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+// Day 5 of 30
+
+
+
+public class Day05 {
+
+    // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on
+    // the language ) that receive a list of integers as input, and return the largest and lowest
+    // number in that list, respectively.
+
+    // Examples (Input -> Output)
+    // * [4,6,2,1,9,63,-134,566] -> max = 566, min = -134
+    // * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+    // * [42, 54, 65, 87, 0] -> min = 0, max = 87
+    // * [5] -> min = 5, max = 5
+    // Notes
+    // You may consider that there will not be any empty arrays/vectors.
+
+    public static int min(int[] list) {
+
+        Arrays.sort(list);
+
+        return list[0];
+
+    }
+
+    public static int max(int[] list) {
+
+        Arrays.sort(list);
+
+        return list[list.length - 1];
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] testOne = {4, 6, 2, 1, 9, 63, -134, 566};
+        int[] testTwo = {4};
+
+        System.out.println(min(testOne));
+        System.out.println(max(testOne));
+        System.out.println(min(testTwo));
+        System.out.println(max(testTwo));
+
+    }
+
+}
