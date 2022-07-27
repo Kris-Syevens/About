0499db978b5f5ad8c31a250528f7d855f2f598ee
Kris <kris@syevens.com>
2022-07-27 01:26:55 -0500

    Update Commit - Java

---
+++ Repo-Files/30-Days-Java/Day18.java
@@ -0,0 +1,24 @@
+// Day 18 of 30
+
+
+public class Day18 {
+    // Notes
+
+    // DESCRIPTION:
+    // Write a function which converts the input string to uppercase.
+
+    public static String uppercaseMe(String s) {
+
+        return s.toUpperCase();
+
+    }
+
+    // Testing notes below:
+    public static void main(String[] args) {
+
+        System.out.println(uppercaseMe("Kristofer")); // KRISTOFER
+        System.out.println(uppercaseMe("Hello World")); // HELLO WORLD
+
+    }
+
+}
