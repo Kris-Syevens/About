9eece74106a1534d464f19c3f3c82d70bd4d8310
Kris <kris@syevens.com>
2022-08-19 02:29:08 -0500

    Initial Commit - TS

---
+++ Repo-Files/30-Days-TypeScript/Day11.ts
@@ -0,0 +1,15 @@
+// Day 11 of 30 Day Challenge
+
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function boolToString(bool: boolean): string {
+  return `${bool}`;
+}
+
+console.log(true); // true
+console.log(false); // false
+console.log(boolToString(true)); // "true"
+console.log(boolToString(false)); // "false"
